89d7b7f14bb7b7b731f0c9e492a7210a459fb2c3
ProgressAce <daiyaansallie7@gmail.com>
2023-11-06 12:26:02 -0800

    add: basic endpoint for flask api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,37 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
+    """displays a status message."""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def object_count():
+    """displays the number of objects by type."""
+
+    types = {
+            "amenities": Amenity,
+            "cities": City,
+            "places": Place,
+            "reviews": Review,
+            "states": State,
+            "users": User
+            }
+
+    object_count = {}
+    for t, cls in types.items():
+        count = storage.count(cls)
+        object_count[t] = count
+
+    return jsonify(object_count)
